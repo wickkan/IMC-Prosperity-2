c6c88312cd36720024710ba168482d29f12d0677
Kanishka Wickramasinghe <kanishkaw@Kanishkas-MacBook-Pro.local>
2024-04-02 23:25:55 +1100

    added datamodel.py file

--- tutorial.py
+++ tutorial.py
@@ -1,4 +1,4 @@
-# sample trader class
+# trader class example
 
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
@@ -8,10 +8,9 @@ import string
 class Trader:
 
     def run(self, state: TradingState):
+        # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
         print("traderData: " + state.traderData)
         print("Observations: " + str(state.observations))
-
-        # Orders to be placed on exchange matching engine
         result = {}
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
@@ -37,10 +36,8 @@ class Trader:
 
             result[product] = orders
 
-            # String value holding Trader state data required.
-        # It will be delivered as TradingState.traderData on next execution.
+        # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
         traderData = "SAMPLE"
 
-        # Sample conversion request. Check more details below.
         conversions = 1
         return result, conversions, traderData
